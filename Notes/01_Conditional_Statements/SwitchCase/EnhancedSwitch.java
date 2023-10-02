abeebd3348bb0ac6dd0acd01a8897264ce273ea7
Anchal Singh <anchals0915@gmail.com>
2023-10-02 12:38:22 +0530

    Added EnhancedSwitch.java

---
+++ Notes/01_Conditional_Statements/SwitchCase/EnhancedSwitch.java
@@ -0,0 +1,30 @@
+package SwitchCase;
+
+import java.util.Scanner;
+
+public class EnhancedSwitch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String fruit = sc.next();
+
+        switch (fruit) {
+            case "Mango" -> System.out.println("King of fruits ");
+            case "Apple" -> System.out.println("a sweet red fruit");
+            case "Orange" -> System.out.println("round fruit");
+            case "Grapes" -> System.out.println("small fruit");
+            default -> System.out.println("Please enter a valid fruit");
+        }
+
+        sc.close();
+    }
+
+}
+
+// Note:
+// * cases have to be the same type as expression must be a constant or literal
+// * duplicate case values are not allowed
+// * break is use to terimate the sequence
+// * if break is not used it will continuew to next case
+
+// break is used to terimate the
+// == checks the refernces of the String
