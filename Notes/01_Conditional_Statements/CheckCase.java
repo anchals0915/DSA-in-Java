f64a74b031e27d969238e09fb37e378de6c729a3
Anchal Singh <anchals0915@gmail.com>
2023-10-02 12:28:39 +0530

    Added CheckCase.java

---
+++ Notes/01_Conditional_Statements/CheckCase.java
@@ -0,0 +1,18 @@
+
+// Q.) Check whether the entered value is lowercase or uppercase
+
+import java.util.Scanner;
+
+public class CheckCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().trim().charAt(0);
+        if (ch >= 'A' && ch <= 'Z') {
+            System.out.println("Uppercase");
+        }
+        if (ch >= 'a' && ch <= 'z') {
+            System.out.println("Lowercase");
+        }
+        sc.close();
+    }
+}
