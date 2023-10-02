5d8f8f6b6426383589f9bb36ad3dcd00b0c175a2
Anchal Singh <anchals0915@gmail.com>
2023-10-02 12:35:30 +0530

    Added FibonnaciNumber.java

---
+++ Notes/02_Loops/FibonnaciNumber.java
@@ -0,0 +1,13 @@
+// Nth Fibonnaci Number
+
+import java.util.Scanner;
+
+// A number is Fibonacci if and only if one or both of (5*n2 + 4) or (5*n2 – 4) is a perfect square
+public class FibonnaciNumber {
+    public static void main(String[] args) 
+        {
+            Scanner sc =new Scanner(System.in);
+            
+            sc.close();
+        }
+}
