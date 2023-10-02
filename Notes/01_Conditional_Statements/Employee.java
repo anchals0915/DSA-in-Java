d6ab7ee873d7b56a7826e7e7717faab7b22f094b
Anchal Singh <anchals0915@gmail.com>
2023-10-02 12:29:22 +0530

    Added Employee.java

---
+++ Notes/01_Conditional_Statements/Employee.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Employee {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int empID = sc.nextInt();
+        String department = sc.next();
+        //////////////////////////////////////////////////////////////////////
+        switch (empID) {
+            case 1:
+                System.out.println("Anchal Singh");
+                break;
+            case 2:
+                System.out.println("Vinita Singh");
+                break;
+            case 3:
+                System.out.println("Empolyee No 3");
+                switch (department) {
+                    case "IT":
+                        System.out.println("IT Department ");
+                        break;
+
+                    case "CSE":
+                        System.out.println("CSE Department ");
+                        break;
+
+                    case "EEE":
+                        System.out.println("EEE Department ");
+
+                }
+                break;
+
+            default:
+                break;
+        }
+        //////////////////////////////////////////////////////////////////////
+        // better way to write
+        switch (empID) {
+            case 1 -> System.out.println("Anchal Singh");
+            case 2 -> System.out.println("Vinita Singh");
+            case 3 -> {
+                System.out.println("Empolyee No 3");
+                switch (department) {
+                    case "IT" -> System.out.println("IT Department ");
+
+                    case "CSE" -> System.out.println("CSE Department ");
+
+                    case "EEE" -> System.out.println("EEE Department ");
+
+                }
+            }
+            default -> System.out.println("Enter a valid data.");
+        }
+
+        sc.close();
+    }
+}
