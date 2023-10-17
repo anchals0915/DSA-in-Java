0752071d29816485d74b41fad103c5c5bb422285
Anchal Singh <anchals0915@gmail.com>
2023-10-17 10:29:10 +0530

    if-else Statement

---
+++ Notes/05_Conditions Loops/Conditionals/Maximum.java
@@ -0,0 +1,36 @@
+// Q: Find the largest of the 3 numbers
+
+import java.util.Scanner;
+
+public class Maximum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+//        int max = a;
+//        if (b > max) {
+//            max = b;
+//        }
+//        if (c > max) {
+//            max = c;
+//        }
+
+//        int max = 0;
+//        if (a > b) {
+//            max = a;
+//        } else {
+//            max = b;
+//        }
+//
+//        if (c > max) {
+//            max = c;
+//        }
+
+        int max = Math.max(c, Math.max(a, b));
+
+        System.out.println("Largest of the 3 numbers : " + max);
+    }
+}
