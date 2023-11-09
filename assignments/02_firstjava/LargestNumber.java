2c26afde95da4da030b938b225b941234b0bb846
Anchal Singh <anchals0915@gmail.com>
2023-11-09 14:10:29 +0530

    Largest of 2 numbers

---
+++ assignments/02_firstjava/LargestNumber.java
@@ -0,0 +1,19 @@
+
+/* Take 2 numbers as input and print the largest number. */
+
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println(largest(num1, num2));
+        sc.close();
+    }
+
+    public static int largest(int num1, int num2) {
+        return num1 > num2 ? num1 : num2;
+    }
+}
