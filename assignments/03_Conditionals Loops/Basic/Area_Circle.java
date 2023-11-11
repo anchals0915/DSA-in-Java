2ff1b7a22041ec62a007d7bb8b6d958731836d91
Anchal Singh <anchals0915@gmail.com>
2023-11-11 14:20:19 +0530

    Area of Circle

--- assignments/03_Conditionals Loops/Basic/Area_Circle.java
+++ assignments/03_Conditionals Loops/Basic/Area_Circle.java
@@ -4,17 +4,19 @@ import java.util.Scanner;
  * Q.) Area Of Circle Java Program
  **/
 
-public class Area_Circle  {
-    public static void main(String[] args) { 
-        Scanner sc = new Scanner(System.in) ;
+public class Area_Circle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter the radius : ");
-        float radius = sc.nextFloat() ; 
-        System.out.println("Area : "+ area(radius));
+        float radius = sc.nextFloat();
+        System.out.println("Area : " + area(radius));
         sc.close();
     }
 
-    // Area of a Circle	Area = πr2	r = radius of the circle
-    static float area( float radius ) {
-        return (float) (Math.PI * Math.pow(radius, 2)) ;
+    // Area of a Circle Area = π*r*r = 
+    static float area(float radius) {
+        // - Formula: A = π * r^2
+        float area = (float) Math.PI * radius * radius;
+        return area;
     }
 }
