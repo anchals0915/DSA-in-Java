e39f983fec002803f5a70493982c8c5f543b4660
Anchal Singh <anchals0915@gmail.com>
2023-11-11 14:20:28 +0530

    Perimeter Of Equilateral Triangle

---
+++ assignments/03_Conditionals Loops/Basic/Perimeter_Equilateral.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * Q.) Perimeter Of Equilateral Triangle
+ **/
+public class Perimeter_Equilateral {
+    public static void main(String[] args) { 
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Side : ");
+        float side = sc.nextFloat();
+
+        System.out.println("Perimeter of Equilateral Triangle :  " + perimeter(side));
+        sc.close();
+
+    }
+
+   
+    static float perimeter( float side ) {
+       // 9. Perimeter of Equilateral Triangle:
+        // - Formula: P = 3 * side
+        float area = 3 * side; 
+        return area ; 
+    }
+}
