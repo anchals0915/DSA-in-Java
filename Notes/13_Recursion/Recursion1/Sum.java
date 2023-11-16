269da7d450bd63989f1e1e2b10ac0b09207f9a5d
Anchal Singh <anchals0915@gmail.com>
2023-11-16 23:55:00 +0530

    Sum of n to 1

---
+++ Notes/13_Recursion/Recursion1/Sum.java
@@ -0,0 +1,16 @@
+
+public class Sum {
+    public static void main(String[] args) {
+        System.out.println(sum(5));
+    }
+
+    /**
+     * Q-04 : Sum of n to 1
+     */
+    static int sum(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n + sum(n - 1);
+    }
+}
