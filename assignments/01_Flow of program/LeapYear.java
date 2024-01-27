a3265482b9b28585bb415f4155bfc2457cf98753
Anchal Singh <anchals0915@gmail.com>
2024-01-27 22:17:00 +0530

    01: leap year or not

---
+++ assignments/01_Flow of program/LeapYear.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Year : ");
+        int year = sc.nextInt();
+
+        if ((year % 400 == 0) || (year % 100 != 0) && (year % 4 == 0)) {
+            System.out.println(" It is a Leap Year");
+        } else {
+            System.out.println(" It is not a Leap Year");
+        }
+
+        sc.close();
+    }
+
+}
